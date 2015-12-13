a8630dc68aaef00b80404e569f7cf7655d656079
owlg <owlg@users.noreply.github.com>
2015-12-13 11:55:38 +0800

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+<?php
+// configuration file to connect to all database services, include all information required to connect, such as database URL, username, password and database name.
+
+$host = '';
+$username = '';
+$password = '';
+$database = '';
