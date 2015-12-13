567055aafb7dba712c1e446456efa662f5fd9576
owlg <owlg@users.noreply.github.com>
2015-12-13 11:58:33 +0800

    Create calRewards.php

---
+++ calRewards.php
@@ -0,0 +1,9 @@
+<?php
+// calculate rewards per view, at 80% conversion rate. Surf points is the total points rewarded when surfing, transferred from the website owner to the user who is surfing the owner's website
+
+$userrewardedPoints = $surfPoints / 100 * 80
+
+//The admin takes 20% of the revenue, to be distributed elsewhere.
+
+$adminrewardedPoints = $surfPoints / 100 * 20
+?>
