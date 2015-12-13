5965dcb77439e7dbfc64acb62f1685ff69ea022d
owlg <owlg@users.noreply.github.com>
2015-12-13 12:32:14 +0800

    Create deductSurfPoints.php

---
+++ deductSurfPoints.php
@@ -0,0 +1,8 @@
+<?php
+// $surfPoints to be deducted from the database, gets information from database for userID to deduct surf points
+
+// set $deductAmt on another script. $finalAmt will be the value of surf points after all deduction, to be modified in database
+
+$totalAmt = $surfPoints - $deductAmt
+
+?>
