6ab8df528f7690caa63aa3593917f7f13d1ac8d7
Chew Jian Yue <owlg@users.noreply.github.com>
2018-09-18 09:13:14 +0800

    Update deductSurfPoints.php
    Code fix

--- deductSurfPoints.php
+++ deductSurfPoints.php
@@ -3,6 +3,7 @@
 
 // set $deductAmt on another script. $finalAmt will be the value of surf points after all deduction, to be modified in database
 function deductPoints(surfPoints, deductAmt) {
-  $totalAmt = $surfPoints - $deductAmt
+  $totalAmt = $surfPoints - $deductAmt;
+    return $totalAmt;
 }
 ?>
