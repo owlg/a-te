9d8f97bb49a88a1151f60c55bf19969321146806
Chew Jian Yue <owlg@users.noreply.github.com>
2018-09-18 09:12:46 +0800

    Update deductSurfPoints.php
    Encapsulate the code with functions, so it can be used on other files

--- deductSurfPoints.php
+++ deductSurfPoints.php
@@ -2,7 +2,7 @@
 // $surfPoints to be deducted from the database, gets information from database for userID to deduct surf points
 
 // set $deductAmt on another script. $finalAmt will be the value of surf points after all deduction, to be modified in database
-
-$totalAmt = $surfPoints - $deductAmt
-
+function deductPoints(surfPoints, deductAmt) {
+  $totalAmt = $surfPoints - $deductAmt
+}
 ?>
